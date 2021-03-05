eba493c86ff6dccab09149b535919f01299550a4
Marco Colombo <marco.colombo@gmail.com>
2021-03-05 16:26:30 +0100

    [MIG] more porting fixes

--- l10n_it_reverse_charge/models/account_rc_type.py
+++ l10n_it_reverse_charge/models/account_rc_type.py
@@ -93,7 +93,6 @@ class AccountRCType(models.Model):
         default=lambda self: self.env.company,
     )
 
-    @api.multi
     @api.constrains("with_supplier_self_invoice", "tax_ids")
     def _check_tax_ids(self):
         for rctype in self:
