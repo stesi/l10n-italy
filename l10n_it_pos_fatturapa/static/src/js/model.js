82987b4ae13da5484a48179ef3a04e7aadcea36d
Francesco Pranzo <pranzo.f@stesi.eu>
2021-11-18 14:12:44 +0100

    update

--- l10n_it_pos_fatturapa/static/src/js/model.js
+++ l10n_it_pos_fatturapa/static/src/js/model.js
@@ -4,6 +4,7 @@ odoo.define('l10n_it_pos_fatturapa.models', function(require) {
     var models = require('point_of_sale.models');
 
     models.load_fields('res.partner', ['pec_destinatario', 'electronic_invoice_subjected',
-                                        'electronic_invoice_obliged_subject', 'codice_destinatario']);
+                                        'electronic_invoice_obliged_subject', 'codice_destinatario',
+                                        'company_type', 'lastname', 'firstname']);
 
 });
