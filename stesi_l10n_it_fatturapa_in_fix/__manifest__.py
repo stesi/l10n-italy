c0b269ff7bc53e3a9e899dbc43c3dadb07da9bdb
FrancescoPranzo-stesi <pranzo.f@stesi.eu>
2022-03-26 14:34:31 +0100

    fix for import of e-invoice from vendor

---
+++ stesi_l10n_it_fatturapa_in_fix/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Stesi FIX ITA - l10n fatturapa in",
+    "version": "14.0.1.0.3",
+    "category": "Localization/Italy",
+    "summary": "Scissione pagamenti",
+    "author": "Abstract, Agile Business Group, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_in",
+    ],
+    "data": [
+
+    ],
+}
+
---
+++ stesi_l10n_it_fatturapa_in_fix/models/wizard_import_fatturapa.py
@@ -0,0 +1,127 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+import re
+
+
+class WizardImportFatturapa(models.TransientModel):
+    _inherit = 'wizard.import.fatturapa'
+
+    def getPartnerBase(self, DatiAnagrafici):  # noqa: C901
+        if not DatiAnagrafici:
+            return False
+        partner_model = self.env["res.partner"]
+        cf = DatiAnagrafici.CodiceFiscale or False
+        vat = False
+        if DatiAnagrafici.IdFiscaleIVA:
+            # Format Italian VAT ID to always have 11 char
+            # to avoid validation error when creating the given partner
+            if DatiAnagrafici.IdFiscaleIVA.IdPaese.upper() == "IT":
+                vat = "{}{}".format(
+                    DatiAnagrafici.IdFiscaleIVA.IdPaese.upper(),
+                    DatiAnagrafici.IdFiscaleIVA.IdCodice.rjust(11, "0")[:11],
+                )
+            else:
+                vat = "{}{}".format(
+                    DatiAnagrafici.IdFiscaleIVA.IdPaese.upper(),
+                    re.sub(r"\W+", "", DatiAnagrafici.IdFiscaleIVA.IdCodice).upper(),
+                )
+        partners = partner_model
+        res_partner_rule = (
+            self.env["ir.model.data"]
+            .sudo()
+            .xmlid_to_object("base.res_partner_rule", raise_if_not_found=False)
+        )
+        if vat:
+            domain = [("vat", "=", vat)]
+            if (
+                self.env.context.get("from_attachment")
+                and res_partner_rule
+                and res_partner_rule.active
+            ):
+                att = self.env.context.get("from_attachment")
+                domain.extend(
+                    [
+                        "|",
+                        ("company_id", "child_of", att.company_id.id),
+                        ("company_id", "=", False),
+                    ]
+                )
+            partners = partner_model.search(domain)
+        if not partners and cf:
+            domain = [("fiscalcode", "=", cf)]
+            if (
+                self.env.context.get("from_attachment")
+                and res_partner_rule
+                and res_partner_rule.active
+            ):
+                att = self.env.context.get("from_attachment")
+                domain.extend(
+                    [
+                        "|",
+                        ("company_id", "child_of", att.company_id.id),
+                        ("company_id", "=", False),
+                    ]
+                )
+            partners = partner_model.search(domain)
+        commercial_partner_id = False
+        if len(partners) > 1:
+            for partner in partners:
+                if (
+                    commercial_partner_id
+                    and partner.commercial_partner_id.id != commercial_partner_id
+                ):
+                    raise UserError(
+                        _(
+                            "Two distinct partners with "
+                            "VAT number %s or Fiscal Code %s already "
+                            "present in db." % (vat, cf)
+                        )
+                    )
+                commercial_partner_id = partner.commercial_partner_id.id
+        if partners:
+            if not commercial_partner_id:
+                commercial_partner_id = partners[0].commercial_partner_id.id
+            self.check_partner_base_data(commercial_partner_id, DatiAnagrafici)
+            return commercial_partner_id
+        else:
+            # partner to be created
+            country_id = False
+            # before to create a partner search for an existing one with the same vat
+            partner = self.env['res.partner'].search([('vat', '=', vat)])
+            if not partner:
+                CountryCode = DatiAnagrafici.IdFiscaleIVA.IdPaese
+                countries = self.CountryByCode(CountryCode)
+                if countries:
+                    country_id = countries[0].id
+                vat_formatted = self.env['res.partner']._fix_vat_number(vat, country_id)
+                partner = self.env['res.partner'].search([('vat', '=', vat_formatted)])
+            if not partner:
+                if DatiAnagrafici.IdFiscaleIVA:
+                    CountryCode = DatiAnagrafici.IdFiscaleIVA.IdPaese
+                    countries = self.CountryByCode(CountryCode)
+                    if countries:
+                        country_id = countries[0].id
+                    else:
+                        raise UserError(
+                            _("Country Code %s not found in system.") % CountryCode
+                        )
+                vals = {
+                    "vat": vat,
+                    "fiscalcode": cf,
+                    "is_company": (
+                        DatiAnagrafici.Anagrafica.Denominazione and True or False
+                    ),
+                    "eori_code": DatiAnagrafici.Anagrafica.CodEORI or "",
+                    "country_id": country_id,
+                }
+                if DatiAnagrafici.Anagrafica.Nome:
+                    vals["firstname"] = DatiAnagrafici.Anagrafica.Nome
+                if DatiAnagrafici.Anagrafica.Cognome:
+                    vals["lastname"] = DatiAnagrafici.Anagrafica.Cognome
+                if DatiAnagrafici.Anagrafica.Denominazione:
+                    vals["name"] = DatiAnagrafici.Anagrafica.Denominazione
+                vals['vat'] = vat
+                return partner_model.create(vals).id
+            else:
+                return partner
+
