6ce50f1ddf5d4f3c57eb329d107edf35859f7ba7
Vincenzo Di Roberto <diroberto.v@stesi.eu>
2022-03-28 13:22:42 +0200

    Problema

--- stesi_l10n_it_fatturapa_in_fix/__manifest__.py
+++ stesi_l10n_it_fatturapa_in_fix/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Stesi FIX ITA - l10n fatturapa in",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "category": "Localization/Italy",
     "summary": "Scissione pagamenti",
     "author": "Abstract, Agile Business Group, Odoo Community Association (OCA)",
