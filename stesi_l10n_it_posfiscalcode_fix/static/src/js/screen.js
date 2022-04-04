34deaea9c1c956b8ecdf47dbc7a5402172aa16e1
FrancescoPranzo-stesi <pranzo.f@stesi.eu>
2022-04-04 09:00:11 +0200

    update of fiscalcode fix

--- stesi_l10n_it_posfiscalcode_fix/static/src/js/screen.js
+++ stesi_l10n_it_posfiscalcode_fix/static/src/js/screen.js
@@ -7,14 +7,27 @@ odoo.define('stesi_l10n_it_posfiscalcode_fix.screens', function(require){
     var { Gui } = require('point_of_sale.Gui');
     var models = require('point_of_sale.models');
 
-    const POSSaveClientOverride = ClientListScreen => class extends ClientListScreen {
-        async saveChanges(event) {
-            var fiscalcode = $('.fiscalcode').val();
-            await this.env.pos.load_new_partners();
-            this.state.selectedClient = this.env.pos.db.get_partner_by_id(partnerId);
-            this.state.detailIsShown = false;
-            this.render();
-        }
+    const POSSaveClientOverride = ClientListScreen =>
+        class extends ClientListScreen {
+            async saveChanges(event) {
+                debugger;
+                var fiscalcode = $('.fiscalcode').val();
+                debugger;
+                //let partnerId = await this.rpc({
+                //model: 'res.partner',
+                //method: 'create_from_ui',
+                //args: [event.detail.processedChanges],
+                //});
+                //debugger;
+                //await this.env.pos.load_new_partners();
+    //            debugger;
+    //            this.state.selectedClient = this.env.pos.db.get_partner_by_id(partnerId);
+    //            debugger;
+                this.state.detailIsShown = false;
+    //            debugger;
+                this.render();
+    //            debugger;
+            }
     };
     Registries.Component.extend(ClientListScreen, POSSaveClientOverride);
     return ClientListScreen;
