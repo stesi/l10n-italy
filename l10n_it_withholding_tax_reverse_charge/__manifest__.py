bae702134145f0bfbc2abb6fccb7995d590046ca
Vincenzo Di Roberto <diroberto.v@stesi.eu>
2022-05-18 12:11:40 +0200

    Withholding Tax Reverse

---
+++ l10n_it_withholding_tax_reverse_charge/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2015 Alessandro Camilli (<http://www.openforce.it>)
+# Copyright 2018 Lorenzo Battistini (https://github.com/eLBati)
+# Copyright 2019 Giovanni - GSLabIt
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "ITA - Ritenute d'acconto-Reverse Charge",
+    "version": "14.0.1.1",
+    "category": "Account",
+    "author": "Stesi",
+    "website": "https://github.com/OCA/l10n-italy",
+    "license": "",
+    "depends": [
+        "l10n_it_withholding_tax",
+        "l10n_it_reverse_charge",
+    ],
+    "data": [
+    ],
+    "qweb": [
+
+    ],
+    "installable": True,
+    "autoinstall": True,
+}
---
+++ l10n_it_withholding_tax_reverse_charge/models/withholding_tax.py
@@ -0,0 +1,45 @@
+# Copyright 2015 Alessandro Camilli (<http://www.openforce.it>)
+# Copyright 2018 Lorenzo Battistini - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import _, api, fields, models
+
+
+class WithholdingTaxStatement(models.Model):
+    """
+    The Withholding tax statement are created at the invoice validation
+    """
+    _inherit = "withholding.tax.statement"
+
+    def get_wt_competence(self, amount_reconcile):
+        dp_obj = self.env["decimal.precision"]
+        amount_wt = 0
+        for st in self:
+            if st.invoice_id:
+                domain = [
+                    ("invoice_id", "=", st.invoice_id.id),
+                    ("withholding_tax_id", "=", st.withholding_tax_id.id),
+                ]
+                wt_inv = self.env["account.invoice.withholding.tax"].search(
+                    domain, limit=1
+                )
+                if wt_inv:
+                    # Aggiunto per fixare il caso di reverse charge con pagamento automatico
+                    amount_net_pay = st.invoice_id.amount_net_pay
+                    for p in st.invoice_id.payment_id:
+                        amount_net_pay -= p.amount
+
+                    amount_base = st.invoice_id.amount_untaxed * (
+                        amount_reconcile / amount_net_pay
+                    )
+                    base = round(amount_base * wt_inv.base_coeff, 5)
+                    amount_wt = round(
+                        base * wt_inv.tax_coeff, dp_obj.precision_get("Account")
+                    )
+                if st.invoice_id.move_type in ["in_refund", "out_refund"]:
+                    amount_wt = -1 * amount_wt
+            elif st.move_id:
+                tax_data = st.withholding_tax_id.compute_tax(amount_reconcile)
+                amount_wt = tax_data["tax"]
+            return amount_wt
