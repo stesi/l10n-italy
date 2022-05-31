9c8de5b1c62ae1a25fec0ee7dd180f18a63eb0f4
Michele <dicroce.m@stesi.eu>
2022-05-31 11:59:24 +0200

    pos

--- fiscal_epos_print/static/src/js/ChromeWidgets/SetRefundInfoButton.js
+++ fiscal_epos_print/static/src/js/ChromeWidgets/SetRefundInfoButton.js
@@ -1,26 +1,26 @@
-odoo.define("fiscal_epos_print.SetRefundInfoButton", function (require) {
-    "use strict";
+odoo.define('fiscal_epos_print.SetRefundInfoButton', function(require) {
+    'use strict';
 
     var core = require("web.core");
     var _t = core._t;
-    const PosComponent = require("point_of_sale.PosComponent");
-    const ProductScreen = require("point_of_sale.ProductScreen");
-    // Const {useListener} = require("web.custom_hooks");
-    const Registries = require("point_of_sale.Registries");
-    // Const {Gui} = require("point_of_sale.Gui");
-    // const {posbus} = require("point_of_sale.utils");
-    // const PaymentScreen = require("point_of_sale.PaymentScreen");
+    const PosComponent = require('point_of_sale.PosComponent');
+    const ProductScreen = require('point_of_sale.ProductScreen');
+    const { useListener } = require('web.custom_hooks');
+    const Registries = require('point_of_sale.Registries');
+    const { Gui } = require('point_of_sale.Gui');
+    const { posbus } = require('point_of_sale.utils');
+    const PaymentScreen = require('point_of_sale.PaymentScreen');
 
     class SetRefundInfoButton extends PosComponent {
-        // Constructor() {
-        // super(...arguments);
-        // useListener('click', this.onClick);
-        // This is from older widget system 12.0
-        // this.pos.bind('change:selectedOrder',function(){
-        //     this.orderline_change();
-        //     this.bind_order_events();
-        // },this);
-        // }
+        constructor() {
+            super(...arguments);
+            // useListener('click', this.onClick);
+            // This is from older widget system 12.0
+            // this.pos.bind('change:selectedOrder',function(){
+            //     this.orderline_change();
+            //     this.bind_order_events();
+            // },this);
+        }
 
         mounted() {
             this.bind_order_events();
@@ -28,106 +28,98 @@ odoo.define("fiscal_epos_print.SetRefundInfoButton", function (require) {
         }
 
         is_available() {
-            const order = this.env.pos.get_order();
-            return order;
+           const order = this.env.pos.get_order();
+           return order
         }
 
         render() {
             var color = this.refund_get_button_color();
-            $(this.el).css("background", color);
+            $(this.el).css('background', color);
         }
 
-        // WillUnmount() {
+        // willUnmount() {
         //     this.env.pos.get('orders').off('add remove change', null, this);
         //     this.env.pos.off('change:selectedOrder', null, this);
         // }
         async onClickRefund() {
             var self = this;
             var current_order = this.env.pos.get_order();
-            this.showPopup("RefundInfoPopup", {
-                title: _t("Refund Information Details"),
+            this.showPopup('RefundInfoPopup', {
+                title: _t('Refund Information Details'),
                 refund_date: current_order.refund_date,
                 refund_report: current_order.refund_report,
                 refund_doc_num: current_order.refund_doc_num,
                 refund_cash_fiscal_serial: current_order.refund_cash_fiscal_serial,
-                update_refund_info_button: function () {
-                    self.render();
+                update_refund_info_button: function(){
+                  self.render();
                 },
             });
         }
 
         bind_order_events() {
+            var self = this;
             var order = this.env.pos.get_order();
 
             if (!order) {
                 return;
             }
 
-            if (this.old_order) {
-                this.old_order.unbind(null, null, this);
+            if(this.old_order) {
+                this.old_order.unbind(null,null,this);
             }
 
-            this.env.pos.bind("change:selectedOrder", this.orderline_change, this);
+            this.env.pos.bind('change:selectedOrder', this.orderline_change, this);
 
             var lines = order.orderlines;
-            lines.unbind("add", this.orderline_change, this);
-            lines.bind("add", this.orderline_change, this);
-            lines.unbind("remove", this.orderline_change, this);
-            lines.bind("remove", this.orderline_change, this);
-            lines.unbind("change", this.orderline_change, this);
-            lines.bind("change", this.orderline_change, this);
+                lines.unbind('add',     this.orderline_change, this);
+                lines.bind('add',       this.orderline_change, this);
+                lines.unbind('remove',  this.orderline_change, this);
+                lines.bind('remove',    this.orderline_change, this);
+                lines.unbind('change',  this.orderline_change, this);
+                lines.bind('change',    this.orderline_change, this);
 
             this.old_order = order;
         }
 
         refund_get_button_color() {
             var order = this.env.pos.get_order();
-            var color = "#e2e2e2";
-            if (order) {
+            var color = '#e2e2e2';
+            if(order) {
                 var lines = order.orderlines;
-                var has_refund =
-                    lines.find(function (line) {
-                        return line.quantity < 0.0;
-                    }) !== undefined;
-                if (has_refund === true) {
-                    if (
-                        order.refund_date &&
-                        order.refund_date !== "" &&
-                        order.refund_doc_num &&
-                        order.refund_doc_num !== "" &&
-                        order.refund_cash_fiscal_serial &&
-                        order.refund_cash_fiscal_serial !== "" &&
-                        order.refund_report &&
-                        order.refund_report !== ""
-                    ) {
-                        color = "lightgreen";
-                    } else {
-                        color = "red";
+                var has_refund = lines.find(function(line){ return line.quantity < 0.0;}) != undefined;
+                if (has_refund == true)
+                {
+                    if (order.refund_date && order.refund_date != '' && order.refund_doc_num && order.refund_doc_num != '' &&
+                        order.refund_cash_fiscal_serial && order.refund_cash_fiscal_serial != '' && order.refund_report && order.refund_report != '') {
+                            color = 'lightgreen';
+                    }
+                    else
+                    {
+                        color = 'red';
                     }
                 }
             }
             return color;
         }
 
-        orderline_change() {
+        orderline_change(){
             var order = this.env.pos.get_order();
             if (order) {
                 var lines = order.orderlines;
-                order.has_refund =
-                    lines.find(function (line) {
-                        return line.quantity < 0.0;
-                    }) !== undefined;
+                order.has_refund = lines.find(function(line){ return line.quantity < 0.0;}) != undefined;
             }
             this.render();
         }
-    }
 
-    SetRefundInfoButton.template = "SetRefundInfoButton";
+
+   }
+
+    SetRefundInfoButton.template = 'SetRefundInfoButton';
 
     ProductScreen.addControlButton({
         component: SetRefundInfoButton,
-        condition: function () {
-            //            Return true;
+        condition: function() {
+//            return true;
             return this.env.pos;
         },
     });
--- fiscal_epos_print/static/src/js/popups.js
+++ fiscal_epos_print/static/src/js/popups.js
@@ -1,18 +1,19 @@
 odoo.define("fiscal_epos_print.popups", function (require) {
     "use strict";
 
-    require("web.core");
-    var popups = require("point_of_sale.popups");
-    var gui = require("point_of_sale.Gui");
+    var core = require("web.core");
+    var popups = require('point_of_sale.popups');
+    var gui = require('point_of_sale.Gui');
+    var _t = core._t;
 
-    // Function addPadding(str, padding = 4) {
-    //     var pad = new Array(padding).fill(0).join("") + str;
-    //     return pad.substr(pad.length - padding, padding);
-    // }
+    function addPadding(str, padding=4) {
+        var pad = new Array(padding).fill(0).join('') + str;
+        return pad.substr(pad.length - padding, padding);
+    }
 
     var RefundInfoPopupWidget = popups.extend({
-        template: "RefundInfoPopupWidget",
-        init: function (parent) {
+        template: 'RefundInfoPopupWidget',
+        init: function(parent) {
             this.refund_report = null;
             this.refund_date = null;
             this.refund_doc_num = null;
@@ -20,52 +21,44 @@ odoo.define("fiscal_epos_print.popups", function (require) {
             this.datepicker = null;
             return this._super(parent);
         },
-        show: function (options) {
-            var opts = options || {};
-            this._super(opts);
-            this.update_refund_info_button = opts.update_refund_info_button;
+        show: function(options){
+            options = options || {};
+            this._super(options);
+            this.update_refund_info_button = options.update_refund_info_button;
             this.renderElement();
             this.datepicker = null;
-            this.$("refund_report").focus();
+            this.$('refund_report').focus();
             this.initializeDatePicker();
         },
-        click_confirm: function () {
+        click_confirm: function(){
             var self = this;
             function allValid() {
-                return self
-                    .$("input")
-                    .toArray()
-                    .every(function (element) {
-                        return element.value && element.value !== "";
-                    });
+                return self.$('input').toArray().every(function(element) {
+                    return element.value && element.value != ''
+                })
             }
 
             if (allValid()) {
-                this.$("#error-message-dialog").hide();
+                this.$('#error-message-dialog').hide()
 
                 var order = this.pos.get_order();
-                order.refund_report = this.$("#refund_report").val();
-                order.refund_date = this.$("#refund_date").val();
-                order.refund_doc_num = this.$("#refund_doc_num").val();
-                order.refund_cash_fiscal_serial = this.$(
-                    "#refund_cash_fiscal_serial"
-                ).val();
+                order.refund_report = this.$('#refund_report').val();
+                order.refund_date = this.$('#refund_date').val();
+                order.refund_doc_num = this.$('#refund_doc_num').val();
+                order.refund_cash_fiscal_serial = this.$('#refund_cash_fiscal_serial').val();
                 this.gui.close_popup();
-                if (
-                    this.update_refund_info_button &&
-                    this.update_refund_info_button instanceof Function
-                ) {
+                if (this.update_refund_info_button && this.update_refund_info_button instanceof Function) {
                     this.update_refund_info_button();
                 }
             } else {
-                this.$("#error-message-dialog").show();
+                this.$('#error-message-dialog').show()
             }
         },
-        initializeDatePicker: function () {
-            var element = this.$("#refund_date").get(0);
+        initializeDatePicker: function() {
+            var self = this,
+                element = this.$('#refund_date').get(0);
 
             if (element && !this.datepicker) {
-                // eslint-disable-next-line
                 this.datepicker = new Pikaday({
                     field: element,
                 });
@@ -74,48 +67,43 @@ odoo.define("fiscal_epos_print.popups", function (require) {
     });
 
     var LotteryCodePopupWidget = popups.extend({
-        template: "LotteryCodePopupWidget",
-        init: function (parent) {
+        template: 'LotteryCodePopupWidget',
+        init: function(parent) {
             this.lottery_code = null;
             return this._super(parent);
         },
-        show: function (options) {
-            var opts = options || {};
-            this._super(opts);
-            this.update_lottery_info_button = opts.update_lottery_info_button;
+        show: function(options){
+            options = options || {};
+            this._super(options);
+            this.update_lottery_info_button = options.update_lottery_info_button;
             this.renderElement();
-            this.$("#lottery_code").focus();
+            this.$('#lottery_code').focus();
         },
         // TODO automatically close popup on barcode scanned
-        click_confirm: function () {
+        click_confirm: function(){
             var self = this;
             function allValid() {
-                return self
-                    .$("input")
-                    .toArray()
-                    .every(function (element) {
-                        return element.value && element.value !== "";
-                    });
+                return self.$('input').toArray().every(function(element) {
+                    return element.value && element.value != ''
+                })
             }
 
             if (allValid()) {
-                this.$("#lottery-error-message-dialog").hide();
+                this.$('#lottery-error-message-dialog').hide()
 
                 var order = this.pos.get_order();
-                order.lottery_code = this.$("#lottery_code").val();
+                order.lottery_code = this.$('#lottery_code').val();
                 this.gui.close_popup();
-                if (
-                    this.update_lottery_info_button &&
-                    this.update_lottery_info_button instanceof Function
-                ) {
+                if (this.update_lottery_info_button && this.update_lottery_info_button instanceof Function) {
                     this.update_lottery_info_button();
                 }
             } else {
-                this.$("#lottery-error-message-dialog").show();
+                this.$('#lottery-error-message-dialog').show()
             }
         },
     });
 
-    gui.define_popup({name: "refundinfo", widget: RefundInfoPopupWidget});
-    gui.define_popup({name: "lotterycode", widget: LotteryCodePopupWidget});
+    gui.define_popup({name:'refundinfo', widget: RefundInfoPopupWidget});
+    gui.define_popup({name:'lotterycode', widget: LotteryCodePopupWidget});
+
 });
