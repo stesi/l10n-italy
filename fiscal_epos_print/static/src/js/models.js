487d7e051cdc15b1a3fc92d49fc19759f0860c8a
Michele <dicroce.m@stesi.eu>
2022-06-13 10:11:30 +0200

    weight

--- fiscal_epos_print/static/src/js/models.js
+++ fiscal_epos_print/static/src/js/models.js
@@ -187,24 +187,24 @@ odoo.define("fiscal_epos_print.models", function (require) {
                 if (!t) {
                     return;
                 }
-                var tax_amount = self._compute_all(t[0], base, quantity);
+                var tax_amount = self._compute_all(t, base, quantity);
                 tax_amount = round_pr(tax_amount, currency_rounding);
                 if (!tax_amount) {
                     // Intervene here: also add taxes with 0 amount
-                    if (t[0].price_include) {
+                    if (t.price_include) {
                         total_excluded -= tax_amount;
                         base -= tax_amount;
                     }
                     // Else {
                     //     total_included += tax_amount;
                     // }
-                    if (t[0].include_base_amount) {
+                    if (t.include_base_amount) {
                         base += tax_amount;
                     }
                     var data = {
-                        id: t[0].id,
+                        id: t.id,
                         amount: tax_amount,
-                        name: t[0].name,
+                        name: t.name,
                     };
                     list_taxes.push(data);
                 }
