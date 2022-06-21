4cada7266322cccd75808a5a97fb9d8bcb219b6f
Vincenzo Di Roberto <diroberto.v@stesi.eu>
2022-06-21 13:14:50 +0200

    Rimosso domain is_trip in vehicle manager

--- l10n_it_withholding_tax_fix_payment/models/account.py
+++ l10n_it_withholding_tax_fix_payment/models/account.py
@@ -6,9 +6,45 @@ from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools.float_utils import float_compare, float_round
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def button_draft(self):
+        account_wt = self.line_ids.filtered(lambda line: line.account_id.user_type_id.type in (
+            'receivable', 'payable')).matched_credit_ids.credit_move_id.filtered(
+            lambda l1: l1.filtered(lambda l1: l1.withholding_tax_generated_by_move_id.id > 0)).move_id
+        if len(account_wt) > 0:
+            account_wt.posted_before = False
+            #account_wt.button_draft()
+            account_wt.button_cancel()
+
+            # reconcile = self.line_ids.filtered(lambda line: line.account_id.user_type_id.type in (
+            # 'receivable', 'payable')).matched_credit_ids.credit_move_id.filtered(
+            # lambda l1: l1.filtered(lambda l1: l1.withholding_tax_generated_by_move_id.id > 0))
+            # if len(reconcile):
+            #     reconcile.unlink()
+            # account_wt.unlink()
+
+            wt_moves = self.line_ids.filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))\
+                .mapped('matched_credit_ids.wt_tax_moves')
+            for wt in wt_moves:
+                wt.unlink()
+            domain = [("move_id", "=", self.id)]
+            wt_statements = self.env["withholding.tax.statement"].search(domain)
+            for wt in wt_statements:
+                wt.unlink()
+        # wt_lines = self.withholding_tax_line_ids
+        # for wt in wt_lines:
+        #     wt.unlink()
+
+
+        return super(AccountMove, self).button_draft()
+
+
 class AccountPartialReconcile(models.Model):
     _inherit = "account.partial.reconcile"
 
+    wt_tax_moves = fields.One2many('withholding.tax.move','reconcile_partial_id', "Wt tax moves")
     @api.model
     def generate_wt_moves(self):
         wt_statement_obj = self.env["withholding.tax.statement"]
@@ -22,6 +58,7 @@ class AccountPartialReconcile(models.Model):
 
         # Search statements of competence
         wt_statements = wt_statement_obj.browse()
+
         rec_line_statement = rec_line_model.browse()
         for rec_line in rec_lines:
             domain = [("move_id", "=", rec_line.move_id.id)]
