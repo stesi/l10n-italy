a6e14c1e3970b688071cedecf44d1f5547ebf067
Michele <dicroce.m@stesi.eu>
2022-06-22 08:24:42 +0200

    state gtms

--- l10n_it_account_margin_tax/models/account_move.py
+++ l10n_it_account_margin_tax/models/account_move.py
@@ -40,6 +40,7 @@ class AccountMove(models.Model):
                 tax_id = margin_tax_lines.mapped('tax_ids').filtered(lambda t: t.margin_tax)
                 base_amount = sum(
                     margin_tax_lines.filtered(lambda t: t.margin > 0).mapped('margin'))
+                tax_base_amount = am.amount_untaxed
                 tax_amount = base_amount* tax_id.orig_percentage / 100
                 invoice_repartition_line_id = tax_id.invoice_repartition_line_ids.filtered(
                     lambda l: l.factor_percent == 100 and l.repartition_type == 'tax')
@@ -71,7 +72,7 @@ class AccountMove(models.Model):
                     "credit": tax_amount,
                     "exclude_from_invoice_tab": True,
                     "is_margin_tax_line": True,
-                    "tax_base_amount": base_amount,
+                    "tax_base_amount": tax_base_amount,
                     "tax_repartition_line_id": invoice_repartition_line_id,
                     # 'move_id': am.id
                 }
