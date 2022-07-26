da482db1c5b01fed1d3b5123a025df2fb12423fd
Gabriele Portente <me@gabrieleportente.it>
2022-07-26 06:55:52 -0700

    update

---
+++ l10n_it_vat_registries_fix/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "ITA - Registri IVA Fix",
+
+    'summary': "",
+
+    'license': 'OPL-1',
+
+    'author': "STeSI Srl",
+
+    'category': '',
+
+    'version': '14.0.0.1',
+
+    'website': "http://www.stesi.eu",
+
+    # any module necessary for this one to work correctly
+    'depends': ['l10n_it_vat_registries'],
+
+    # always loaded
+    'data': [],
+
+    'application': False,
+}
