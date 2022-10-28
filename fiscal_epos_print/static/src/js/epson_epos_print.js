32c5a0c5213efba572d46df0693d4ca3507b9b2d
Gabriele Portente <me@gabrieleportente.it>
2022-10-28 10:47:05 +0200

    ripristinato parte di codice pos

--- fiscal_epos_print/static/src/js/epson_epos_print.js
+++ fiscal_epos_print/static/src/js/epson_epos_print.js
@@ -560,12 +560,12 @@ odoo.define("fiscal_epos_print.epson_epos_print", function (require) {
                         });
 
                         // TODO This line of code is added by us, check if it's right
-                        // xml += self.printRecItem({
-                        //    description: _t('Refund cash'),
-                        //    quantity: l.quantity * -1.0,
-                        //    unitPrice: l.price,
-                        //    department: l.tax_department.code
-                        // });
+                        xml += self.printRecItem({
+                            description: _t('Refund cash'),
+                            quantity: l.quantity * -1.0,
+                            unitPrice: l.price,
+                            department: l.tax_department.code
+                        });
                     }
                 } else {
                     xml += self.printRecItemAdjustment({
