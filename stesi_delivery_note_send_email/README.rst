37da1bd9ff7eae3f9a4689c95babe10e5a7d9db5
Arcadio <pinto.a@stesi.eu>
2022-11-16 10:51:17 +0100

    update

---
+++ stesi_delivery_note_send_email/README.rst
@@ -0,0 +1,4 @@
+==================
+Stesi Delivery Note Send Email
+==================
+Questo modulo acconsente di inviare tramite email da i documenti di trasporto.
--- stesi_delivery_note_send_email/models/stock_delivery_note.py
+++ stesi_delivery_note_send_email/models/stock_delivery_note.py
@@ -19,7 +19,7 @@ class StockDeliveryNote(models.Model):
             'default_template_id': template_id,
             'default_composition_mode': 'comment',
             'mark_so_as_sent': True,
-            'custom_layout': "mail.mail_notification_paynow",
+            'custom_layout': "mail.mail_notification_light",
             'proforma': self.env.context.get('proforma', False),
             'force_email': True,
             'model_description': self.with_context(lang=lang).name,
