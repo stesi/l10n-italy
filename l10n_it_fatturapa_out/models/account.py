7a2cc135d3baf390658479630497c40c118333ac
Michele <dicroce.m@stesi.eu>
2023-01-04 09:08:34 +0100

    temp comment

--- l10n_it_fatturapa_out/models/account.py
+++ l10n_it_fatturapa_out/models/account.py
@@ -49,11 +49,11 @@ class AccountInvoice(models.Model):
 
     def preventive_checks(self):
         for invoice in self:
-            if not invoice.is_sale_document():
-                raise UserError(
-                    _("Impossible to generate XML: not a customer invoice: %s")
-                    % invoice.name
-                )
+            # if not invoice.is_sale_document():
+            #     raise UserError(
+            #         _("Impossible to generate XML: not a customer invoice: %s")
+            #         % invoice.name
+            #     )
 
             if (
                 invoice.invoice_payment_term_id
