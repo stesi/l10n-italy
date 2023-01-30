953e5f0291fe109f92ec5204f94a93d469c12161
Arcadio <pinto.a@stesi.eu>
2023-01-30 10:11:48 +0100

    update

--- stesi_delivery_note_volume_calculation/README.rst
+++
@@ -1,4 +0,0 @@
-==================
-Stesi Delivery Note Volume Calculation
-==================
-Questo modulo calcola il volume e il peso netto del ddt in maniera automatica.
--- stesi_delivery_note_volume_calculation/models/stock_delivery_note.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-
-
-class StockDeliveryNote(models.Model):
-    _inherit = "stock.delivery.note"
-
-
