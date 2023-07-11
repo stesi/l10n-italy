b9084e7c44242c7f9b98d8345216c521a2328cff
Arcadio <pinto.a@stesi.eu>
2023-07-11 12:41:12 +0200

    aggiornamneto l10n

---
+++ updaterepo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cwd=$(pwd)
+for f in ../repository/*; do
+    if [ -d "$f" ]; then
+        # Will not run if no directories are available
+        #echo "$f"
+        cd $f
+#echo $(pwd)
+        git pull
+        git submodule update --init --remote stesi/l10n-italy
+        git commit -am "Update l10n-italy"
+        git push
+        cd  "$cwd" 
+    fi
+done
